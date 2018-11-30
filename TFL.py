56a6cbb6471c42da460c2bab5106d9f61ba88416
Tom Dyson <tom.dyson@reedbusiness.com>
2018-11-30 17:49:16 +0000

    changed to append to dictionaries

--- TFL.py
+++ TFL.py
@@ -3,10 +3,12 @@ import requests as r
 import pandas as pd
 import json
 import datetime
+import time
 import xml.etree.ElementTree as ET
 import numpy as np
 
 from xml.etree.ElementTree import fromstring
+
 
 class BikeScrape:
     def __init__(self, metadata="metadata.json"):
@@ -14,41 +16,41 @@ class BikeScrape:
         with open(metadata) as f:
             self.metadata = json.load(f)
         self.ids = [i for i in self.metadata.keys()]
+        self.bikes_present = {}
+        self.empty_docks = {}
+        self.total_docks = {}
         # self.cols = ids.insert(0, 'Time')
-        self.bikespresent = pd.DataFrame(columns=self.ids)
-        self.emptydocks = pd.DataFrame(columns=self.ids)
-        self.totaldocks = pd.DataFrame(columns=self.ids)
-    
+        # self.bikespresent = pd.DataFrame(columns=self.ids)
+        # self.emptydocks = pd.DataFrame(columns=self.ids)
+        # self.totaldocks = pd.DataFrame(columns=self.ids)
+
     def scrape(self, maxrows):
         count = 0
         startTime = datetime.datetime.now()
         while count < maxrows:
             runTime = datetime.datetime.now()
-            timeDelta = runTime - startTime
-            print(timeDelta.seconds)
             stamp = runTime.strftime("%Y-%m-%d %H:%M:%S")
-            if timeDelta.seconds % 10 == 0:
-                BikeScrape.capture(self, stamp)
-                count += 1
-            else:
-                continue
+            BikeScrape.capture(self, stamp)
+            count += 1
+            time.sleep(60 * 15)
+
+        return self
 
     def capture(self, stamp):
         ping = r.get(self.url)
         content = fromstring(ping.content)
-        present = []
-        empty = []
-        total = []
+        current_present = []
+        current_empty = []
+        current_total = []
         for station in content.findall("station"):
-            present.append(station.find("nbBikes").text)
-            print(station.find("nbBikes").text)
-            empty.append(station.find("nbEmptyDocks").text)
-            total.append(station.find("nbDocks").text)
-        df1 = pd.DataFrame(present, columns=self.ids)
-        #df2 = pd.DataFrame(empty).transpose()  # , columns=self.ids)
-        #df3 = pd.DataFrame(total).transpose()  # , columns=self.ids)
-        self.bikespresent.append(df1)
-        #self.emptydocks.append(df2)
-        #self.totaldocks.append(df3)
+            current_present.append(station.find("nbBikes").text)
+            current_empty.append(station.find("nbEmptyDocks").text)
+            current_total.append(station.find("nbDocks").text)
+
+        self.bikes_present[stamp] = (current_present.copy())
+        self.empty_docks[stamp] = (current_empty.copy())
+        self.total_docks[stamp] = (current_total.copy())
 
 
+a = BikeScrape().scrape(100)
+print(a.bikes_present)
